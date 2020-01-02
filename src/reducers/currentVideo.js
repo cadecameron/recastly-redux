b0ffaeec037a9e350fa0bdf3899cd2e09044610b
cadecameron <cade.cameron@gmail.com>
2020-01-02 15:29:09 -0800

    Completed basic requirements up to videoList

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,12 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (previousState = initialState, action) => {
+
   //TODO: define a reducer for the currentVideo field of our state.
+
+  return Object.assign({}, previousState, {
+    currentVideo: action.payload
+  });
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,14 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (previousState = sampleData, action) => {
   //TODO: define a reducer for the videoList field of our state.
+  if (action.type === 'CHANGE_VIDEO_LIST') {
+    return Object.assign({}, previousState, {
+      videos: action.payload
+    });
+  } else {
+    return previousState;
+  }
 };
 
 export default videoListReducer;
